eebb46a7778cf6d0b4e1c74098abac69c0374c2f
Drew Jaqua <jaqan02@ca.com>
2017-01-28 17:40:43 -0500

    Added web references to docs folder and provided a name for the bars

---
+++ docs/WebResources.md
@@ -0,0 +1,8 @@
+#Official Documentation
+* [Official Highcharts documentation on React](http://www.highcharts.com/blog/192-use-highcharts-to-create-charts-in-react)
+* [Highcharts Options Reference](http://api.highcharts.com/highcharts/)
+
+##JSFiddle Examples
+* [POJS Funnel Chart - lots of options specified](https://jsfiddle.net/jon_a_nygaard/nzop15s0/)
+* [POJS Bar Chart example](http://jsfiddle.net/gh/get/jquery/3.1.1/highcharts/highcharts/tree/master/samples/highcharts/chart/type-bar/)
+ 
--- src/Chart.js
+++ src/Chart.js
@@ -42,8 +42,9 @@ class Chart extends Component {
                             this.x + ': ' + this.y;
                     }
                 },
-                    series: [{
-                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+                series: [{
+                    name: 'Whatever we\'re charting',
+                    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
                 }]
 		    } // end of options parameter*/
         ); // end this.chart initialization */
